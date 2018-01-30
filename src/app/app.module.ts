73d66dee8b67972263085c8ba5f393fcb1693064
Micah Wells <>
2018-01-30 14:17:39 -0600

    added player and playerList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
+import { PlayerComponent } from './players/player/player.component';
+import { PlayerListComponent } from './players/player-list/player-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayersComponent
+    PlayersComponent,
+    PlayerComponent,
+    PlayerListComponent
   ],
   imports: [
     BrowserModule
